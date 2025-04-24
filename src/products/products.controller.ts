ee99c5306d1efddb590bf394d290654efc3350b2
Lucas Skibinski <lucas_skibinski1@hotmail.com>
2025-04-23 23:27:48 -0300

    Initial commit for backend

---
+++ src/products/products.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  create(@Body() data: CreateProductDto) {
+    return this.productsService.create(data);
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(Number(id));
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(Number(id));
+  }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+
+@Injectable()
+export class ProductsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(data: CreateProductDto) {
+    return this.prisma.product.create({ data });
+  }
+
+  findAll() {
+    return this.prisma.product.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.product.findUnique({ where: { id } });
+  }
+
+  remove(id: number) {
+    return this.prisma.product.delete({ where: { id } });
+  }
+}
